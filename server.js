f0493aebc65f0d39b4687b957245d281852445b6
Vincent Leclerc <23376153+vincent-leclerc-dev@users.noreply.github.com>
2025-12-15 10:59:15 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
-import * as fs from "node:fs";
 import * as http from "node:http";
-import * as path from "node:path";
 import {spawn, spawnSync} from 'child_process';
+
+function serverResponse(res, status=200, message='success') {
+        res.writeHead(status, { "Content-Type": "application/json"});
+        res.end(JSON.stringify({status, message}));
+        return;
+}
+
+function activateGPIO(id, during) {
+        if(!id || !during) {
+                return 'Parameters "id" and "during" are mandatory.';
+        }
+
+        const pythonProcess = spawn('/usr/bin/python', ["/root/vld-rpi-gpio-interface/gpio-rpi3.py", id, during]);
+        pythonProcess.stdout.on('data', (data) => console.log('callback: ', data));
+        return `Command activate GPIO ${id} during ${during}s was executed successfully.`
+}
 
 const PORT = 3000;
 
@@ -12,41 +26,20 @@ http.createServer(async (req, res) => {
         console.log(`input request "${req.url}" at "${(new Date()).toISOString()}"`);
 
         try {
-                const matches = req.url.match('(?<path>.*)?id=(?<id>[0-9]{1,2})&during=(?<during>[0-9]{1,2})');
+                const matches = req.url.match('/gpio/activate?id=(?<id>[0-9]+)&during=(?<during>[0-9]+)');
 
                 if(!matches) {
-                        return response(res, 404, `Route ${req.url} was not found.`);
+                        return serverResponse(res, 404, `Route ${req.url} was not found.`);
                 }
 
-                const { path, id, during } = matches['groups'];
-
-                // remove the last '?'
-                const basePath = path.slice(0, -1);
-
-                if(basePath !== '/gpio/activate') {
-                        return response(res, 404, `Route ${req.url} was not found.`);
-                }
-
-                activateGPIO(id, during);
-
-                return response(res, 200, `Command activate GPIO ${id} during ${during}s was executed successfully.`);
+                const { id, during } = matches['groups'];
+                const message = activateGPIO(id, during);
+                return serverResponse(res, 200, message);
 
         } catch (error) {
-                return response(res, 500, error?.message || 'error');
+                return serverResponse(res, 500, error?.message || 'error');
         }
 
 }).listen(PORT);
 
 console.log(`Server running at http://127.0.0.1:${PORT}/`);
-
-function activateGPIO(id, during) {
-        console.log(`Execute command activate GPIO ${id} during ${during}s`);
-        const pythonProcess = spawn('/usr/bin/python', ["/root/vld-rpi-gpio-interface/gpio-rpi3.py", id, during]);
-        pythonProcess.stdout.on('data', (data) => console.log('callback: ', data));
-}
-
-function response(res, status=200, message='success') {
-        res.writeHead(status, { "Content-Type": "application/json"});
-        res.end(JSON.stringify({status, message}));
-        return;
-}
